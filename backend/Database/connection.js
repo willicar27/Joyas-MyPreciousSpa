8a024fda2ec8024007d60bf505904b37c5ea8b0e
willicar27 <willicar27@gmail.com>
2025-09-17 20:18:16 -0300

    inicio del proyecto, dependencias, index

---
+++ backend/Database/connection.js
@@ -0,0 +1,19 @@
+import pkg from "pg";
+import "dotenv/config";
+const { Pool } = pkg;
+
+export const pool = new Pool ({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+    allowExitOnIdle: true,
+});
+
+try {
+    await pool.query("SELECT NOW()");
+    console.log("Base de datos conectada");
+} catch (error) {
+    console.log(error);
+};
---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`servidor Encendido en el puerto http://localhost:${PORT}`);
+});
